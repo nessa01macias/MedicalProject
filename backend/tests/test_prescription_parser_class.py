eb4021de4f38152204730685ecf278b5a9986bb9
Melany Macias <nessa01macias@gmail.com>
2022-08-24 14:56:52 +0300

    "Implemented testing with the pytest library"

--- backend/tests/test_prescription_parser_class.py
+++ backend/tests/test_prescription_parser_class.py
@@ -1,49 +1,40 @@
 from backend.source.parser_perscription_class import PrescriptionParser
-
+import pytest
+@pytest.fixture()
+def example_text1():
+    text1 = """Name: Marta Sharapova Date: 5/11/2022
+    Address: 9 tennis court, new Russia, DC
+    Prednisone 20 mg
+    Lialda 2.4 gram
+    Directions:
+    Prednisone, Taper 5 mg every 3 days,
+    Finish in 2.5 weeks a
+    Lialda - take 2 pill everyday for 1 month
+    Refill: 2 times
+    """
+    return text1
 
 # unit tests for checking all the functionalities of the prescription class
-def test_get_name():
-    obj = PrescriptionParser(data)
-    assert obj.get_general("patient_name") == "Marta Sharapova"
+def test_get_name(example_text1):
+    assert example_text1.get_general("patient_name") == "Marta Sharapova"
 
 
-def test_get_address():
-    obj = PrescriptionParser(data)
-    assert obj.get_general("patient_address") == "9 tennis court, new Russia, DC"
+def test_get_address(example_text1):
+    assert example_text1.get_general("patient_address") == "9 tennis court, new Russia, DC"
 
 
 def test_get_instruction():
-    obj = PrescriptionParser(data)
-    assert obj.get_general(
-        "patient_instruction") == 'Prednisone, Taper 5 mg every 3 days,\nFinish in 2.5 weeks a\nLialda - take 2 pill everyday for 1 month'
+    assert example_text1.get_general("patient_instruction") == 'Prednisone, Taper 5 mg every 3 days,\nFinish in 2.5 weeks a\nLialda - take 2 pill everyday for 1 month'
 
 
-def test_get_prescription():
-    obj = PrescriptionParser(data)
-    assert obj.get_prescription() == 'Prednisone 20 mg\nLialda 2.4 gram'
+def test_get_prescription(example_text1):
+    assert example_text1.get_prescription() == 'Prednisone 20 mg\nLialda 2.4 gram'
 
 
-def test_get_refill():
-    obj = PrescriptionParser(data)
-    assert obj.get_general("patient_refill") == "2"
+def test_get_refill(example_text1):
+    assert example_text1.get_general("patient_refill") == "2"
 
 
-data = """Name: Marta Sharapova Date: 5/11/2022
-
-Address: 9 tennis court, new Russia, DC
-
-Prednisone 20 mg
-Lialda 2.4 gram
-
-Directions:
-Prednisone, Taper 5 mg every 3 days,
-Finish in 2.5 weeks a
-Lialda - take 2 pill everyday for 1 month
-
-Refill: 2 times
-
-
-"""
 test_get_name()
 test_get_address()
 test_get_instruction()
